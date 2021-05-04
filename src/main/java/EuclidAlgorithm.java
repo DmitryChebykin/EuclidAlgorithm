8f05c1ef47f9f56bb5bc039178e98233f8f1bbc1
Dmitry Chebykin <dim_c@mail.ru>
2021-05-04 12:33:07 +0700

    fix

--- src/main/java/EuclidAlgorithm.java
+++ src/main/java/EuclidAlgorithm.java
@@ -5,22 +5,27 @@ public class EuclidAlgorithm {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Введите целое первое число");
-        int firstNumber = scanner.nextInt();
+        int number1 = scanner.nextInt();
 
         System.out.println("Введите целое второе число");
-        int secondNumber = scanner.nextInt();
+        int number2 = scanner.nextInt();
 
-        int gcd = getGCD(firstNumber, secondNumber);
-        System.out.println("Наибольший общий делитель равен " + gcd);
-    }
-
-    private static int getGCD(int firstNumber, int secondNumber) {
-        while (firstNumber % secondNumber != 0) {
-            int temp = firstNumber;
-            firstNumber = secondNumber;
-            secondNumber = temp % secondNumber;
+        if (number1 == 0 || number2 == 0) {
+            System.out.println("Когда одно из чисел равно 0, НОД этих чисел не ищется.");
+            return;
         }
 
-        return secondNumber;
+        int greatestCommonDivisor = getGreatestCommonDivisor(number1, number2);
+        System.out.printf("Наибольший общий делитель равен " + greatestCommonDivisor);
+    }
+
+    private static int getGreatestCommonDivisor(int number1, int number2) {
+        while (number1 % number2 != 0) {
+            int temp = number1;
+            number1 = number2;
+            number2 = temp % number2;
+        }
+
+        return number2;
     }
 }
